19c827e6a25ca3abb5fb47c4ff94918b3396ef62
Christina Hall <cmcclai26@gmail.com>
2023-02-04 18:27:35 -0500

    showing data from city in log, need to pull lat and lon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,38 @@
 var APIKey = '4532a034efa99547e19a1354eab8e09d';
 var searchBtn = $('#searchBtn');
-var lat;
-var lon;
 
-var currentCityDisplay = function (city) {
-    var city = $('.form-control').val();
-    // var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var currentCityDisplay = function () {
+    var cityInput = $('#input-city').val().trim();
+    var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityInput + "&limit=5" + "&appid=" + APIKey;
+
+    fetch(queryURL)
+
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+
+    currentCityForecast(cityInput);
+};
+
+var currentCityForecast = function (cityInput) {
+    console.log(cityInput);
+    var lat = data[0].lat;
+    var lon = data[0].lon;
     var geoTag = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
 
     fetch(geoTag)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        for (var i = 0; i < data.length; i++) {
-            console.log(data[i]);
-        }
-    })
 
-    };
+        .then(function (response) {
+            return response.json();
+        })
+
+        .then(function (data) {
+            console.log(data);
+        })
+}
 
 
 searchBtn.on('click', function () {
